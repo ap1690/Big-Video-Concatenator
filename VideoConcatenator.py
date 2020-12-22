6901ef6eaf813f91ffe0316fc4ecf07906fd4430
Akshey Prabhakar <41077674+ap1690@users.noreply.github.com>
2020-12-23 03:04:21 +0530

    Update VideoConcatenator.py

--- VideoConcatenator.py
+++ VideoConcatenator.py
@@ -1,13 +1,13 @@
 import pytube
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 # We can add more links in below array which we want to get it concatinated
-link=["https://www.youtube.com/watch?v=COJroHpBjl4&feature=youtu.be&ab_channel=PulkitMidha","https://www.youtube.com/watch?v=ZyeY5rqCRz4&feature=youtu.be&ab_channel=PulkitMidha"]
+link=["https://www.youtube.com/watch?v=C","https://www.youtube.com/fdvdfbvs"]
 for i in link:
   yt = pytube.YouTube(i)
   stream = yt.streams.first()
   stream.download()
-clip_1 = VideoFileClip("dhcv.mp4")
-clip_2 = VideoFileClip("DHCV2.mp4")
+clip_1 = VideoFileClip("video1.mp4")
+clip_2 = VideoFileClip("video2.mp4")
 final_clip = concatenate_videoclips([clip_1,clip_2])
 final_clip.write_videofile("final.mp4")
 
